c5fe6fc46aa58ba612c9fb86aa137a12ce8f8b14
Satoshi Umeda <umeda0@gmail.com>
2017-08-31 00:23:19 +0900

    test commit

---
+++ source/js/scripts.js
@@ -0,0 +1,1 @@
+!function(){for(var t=document.querySelectorAll("a"),e=0;e<t.length;e++)t[e].host!==location.host&&(t[e].setAttribute("target","_blank"),t[e].setAttribute("rel","noopener noreferrer"))}(),$(function(){$(".projects h3").click(function(){$(this).next("div").slideToggle()})});
